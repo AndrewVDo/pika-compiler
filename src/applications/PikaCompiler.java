302fc1021508041cf9af960c7945023b07a96497
Andrew Do <andrew.do1613@gmail.com>
2020-05-21 13:52:21 -0700

    comment out optimize import

--- src/applications/PikaCompiler.java
+++ src/applications/PikaCompiler.java
@@ -5,7 +5,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 
-import optimizer.Optimizer;
+//import optimizer.Optimizer;
 import asmCodeGenerator.ASMCodeGenerator;
 import asmCodeGenerator.codeStorage.ASMCodeFragment;
 import lexicalAnalyzer.LexicalAnalyzer;
