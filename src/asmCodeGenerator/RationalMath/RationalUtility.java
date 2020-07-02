5b749364c9f457c0982764e9bcca70475de7ebeb
Andrew Do <Andrew.do1613@gmail.com>
2020-07-01 20:32:42 -0700

    rational add working

--- src/asmCodeGenerator/RationalMath/RationalUtility.java
+++ src/asmCodeGenerator/RationalMath/RationalUtility.java
@@ -5,7 +5,9 @@ import asmCodeGenerator.SimpleCodeGenerator;
 import asmCodeGenerator.codeStorage.ASMCodeFragment;
 import parseTree.ParseNode;
 
+import static asmCodeGenerator.RationalGenerator.RATIONAL_GCD;
 import static asmCodeGenerator.codeStorage.ASMOpcode.*;
+import static asmCodeGenerator.runtime.RunTime.RATIONAL_TEMP;
 
 public class RationalUtility implements SimpleCodeGenerator {
     @Override
@@ -24,7 +26,6 @@ public class RationalUtility implements SimpleCodeGenerator {
         Macros.declareF(frag, functionName + "-res"); //return util loads this result at the end
         frag.add(Label, functionName);
             Macros.storeITo(frag, functionName + "-caller");
-            frag.add(PStack);
             Macros.storeFTo(frag, functionName + "-raw-right");
             Macros.storeFTo(frag, functionName + "-raw-left");
             frag.add(PushD, functionName + "-raw-left");
@@ -35,12 +36,33 @@ public class RationalUtility implements SimpleCodeGenerator {
                 Macros.storeITo(frag, functionName + "-b");
             frag.add(PushD, functionName + "-raw-right");
                 Macros.readIOffset(frag, 0);
-                Macros.storeITo(frag, functionName + "-a");
+                Macros.storeITo(frag, functionName + "-c");
             frag.add(PushD, functionName + "-raw-right");
                 Macros.readIOffset(frag, 4);
-                Macros.storeITo(frag, functionName + "-b");
+                Macros.storeITo(frag, functionName + "-d");
     }
     public static final void returnUtil(ASMCodeFragment frag, String functionName) {
+        //gcd argument loading
+        frag.add(PushD, functionName + "-res");
+        Macros.readIOffset(frag, 0);
+        frag.add(PushD, functionName + "-res");
+        Macros.readIOffset(frag, 4);
+        frag.add(Call, RATIONAL_GCD);
+            //[... gcd]
+            frag.add(Duplicate);
+                frag.add(PushD, functionName + "-res"); //divide den with gcd & save
+                Macros.readIOffset(frag, 4);
+                frag.add(Exchange);
+                frag.add(Divide);
+                frag.add(PushD, functionName + "-res");
+                Macros.writeIOffset(frag, 4);
+            frag.add(PushD, functionName + "-res"); //divide num with gcd & save
+                Macros.readIOffset(frag, 0);
+                frag.add(Exchange);
+                frag.add(Divide);
+                frag.add(PushD, functionName + "-res");
+                Macros.writeIOffset(frag, 0);
+
         Macros.loadFFrom(frag, functionName + "-res");
         frag.add(Label, functionName + "-return");
         Macros.loadIFrom(frag, functionName + "-caller");
